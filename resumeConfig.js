526dc8a23f231dedaed0c67657bc591e5c1cede9
fov42550564 <42550564@qq.com>
2018-04-11 15:20:37 +0800

    添加resumeConfig

---
+++ resumeConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: '个人简历',
+    name: '简历',
+    path: 'common/resume.md',
+    // path: 'help.js',
+    styles: [],
+    scripts: [],
+    container: {
+        dom: 'div', // default: div
+        props: { className: 'resumeContainer' },
+    },
+    // dist: '.',
+    dist: '1.html',
+};
