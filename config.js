39625680e9a119869b602f959e3344b0b54a57ab
fov42550564 <42550564@qq.com>
2019-09-11 17:10:58 +0800

    修正config

--- config.js
+++ config.js
@@ -1,80 +1,41 @@
 const markdownPlugins = require('./lib/markdownPlugins');
 
 const config = {
-    projectName: 'mdoc-example',
-    title: '四面通物流大超市',
-    favicon: 'img/favicon.ico',
-    logo: 'img/logo.png',
-    colors: {
+    projectName: 'mdoc-example', // 工程名称，访问网站的时候会使用到：http://localhost:4000/mdoc-example
+    title: '四面通物流大超市', // html 的 title
+    favicon: 'img/favicon.ico', // html 的 favicon
+    logo: 'img/logo.png', // logo
+    colors: { // 颜色配置，在这里配置的颜色，在所有的css文件中使用 $primaryColor 来表示一个颜色即可 [可空]
         primaryColor: 'rgb(34, 34, 34)',
         secondaryColor: '#FF8C00',
         activeColor: '#FF4040',
         tintColor: '#005068',
     },
-    highlight: {
+    highlight: { // 代码的风格 [可空]
         theme: 'solarized-dark',
     },
-    markdownPlugins,
-    documentPath: 'root', //默认为doc
-    styles: [],
-    scripts: [],
-    footer: 'lib/Footer.js', //设置footer
+    markdownPlugins,  // 基于 markdown-it 的用户自定义 [可空]
+    documentPath: 'root', //默认为doc [可空]
+    styles: [], // 整个工程的 style，全局有效 [可空]
+    scripts: [], // 整个工程的 script，全局有效 [可空]
+    footer: 'lib/Footer.js', //设置footer [可空]
     sideNavCollapsible: true, // 侧边栏是否可以折叠
-    homePage: {
-        name: 'mdoc的用法',
-        path: 'index.md',
+    homePage: { // 首页 （每个页面都可以配自己的styles和scripts）
+        name: 'mdoc的用法',  // 标题
+        path: 'index.md',  // 文件路径
     },
-    menus: [
-        {
-            name: '帮助',
-            mainPage: 'help.js',
+    menus: [ // 菜单，第一级会显示在顶部菜单栏，menus 包含多个 menu
+        { // 第一个 menu，这个menu没有groups和pages，只显示一个页面
+            name: '帮助', // 标题及菜单名字
+            mainPage: 'help.js', // 该菜单对应的首页
         },
-        {
-            name: '文件夹展示1', // 如果没有该值，则为文件夹的名称 (expand 为 true 时无效)
-            folder: 'dir/menu', // 指定的路径
-            static: false, // 是否为static里面的文件
-            expand: false, // 如果为true,则该文件夹中的子文件夹在顶部展示为各个 menuItem，此时 name 无效，否则，以 name 为 menuItem，子文件夹为 group
-            // origin: 'https://raw.githubusercontent.com/blogoo/photo/master/', // 原始文件的链接
-        },
-        {
-            name: '文件夹展示2',
-            groups: [
+        { // 第二个 menu
+            name: '示例',  // 标题及菜单名字
+            mainPage: 'md/container.md', // 该菜单对应的首页 (必须在pages中能找到)
+            groups: [ // groups 包含多个 group, 一个 group包含一个 name 和 多个页面，都会在左侧菜单中呈现
                 {
-                    name: '文件夹展示实例1', // 如果没有该值，则为文件夹的名称 (expand 为 true 时无效)
-                    folder: 'dir/group', // 指定的路径
-                    static: false, // 是否为static里面的文件
-                    expand: false, // 如果为true,则该文件夹中的子文件夹在顶部展示为各个 group，此时 name 无效，否则，以 name 为 group，子文件夹为 group 的内容
-                    // origin: 'https://raw.githubusercontent.com/blogoo/photo/master/', // 原始文件的链接
-                },
-                {
-                    name: '文件夹展示实例2',
-                    pages: [
-                        {
-                            name: 'fragment',
-                            path: 'md/fragment.md',
-                        },
-                        {
-                            name: '文件夹展示实例3', // 如果没有该值，则为文件夹的名称 (expand 为 true 时无效)
-                            folder: 'dir/page', // 指定的路径
-                            static: false, // 是否为static里面的文件
-                            expand: true, // 如果为true,则该文件夹中的子文件夹在顶部展示为各个 page，此时 name 无效，否则，以 name 为 page，子文件夹为 page 的内容
-                            // origin: 'https://raw.githubusercontent.com/blogoo/photo/master/', // 原始文件的链接
-                        },
-                        {
-                            name: 'fragment',
-                            path: 'md/fragment.md',
-                        },
-                    ]
-                },
-            ]
-        },
-        {
-            name: '示例',
-            mainPage: 'md/container.md',
-            groups: [
-                {
-                    name: '常用',
-                    pages: [
+                    name: '常用', // group的名称
+                    pages: [ // 该group包含的页面
                         {
                             name: 'toc',
                             path: 'common/toc.md',
@@ -93,11 +54,11 @@ const config = {
                             path: 'common/news.md',
                         },
                         {
-                            name: '简历',
-                            path: 'common/resume.md',
-                            container: {
-                                dom: 'div', // default: div
-                                props: { className: 'resumeContainer' },
+                            name: '简历', // 页面的名称及标题
+                            path: 'common/resume.md', // 对应的路径
+                            container: { // 该页面拥有一个自定义的container
+                                dom: 'div', // default: div [可空]
+                                props: { className: 'resumeContainer' }, // 该container的属性
                             }
                         },
                         {
@@ -174,7 +135,7 @@ const config = {
                         {
                             name: 'sequence',
                             path: 'md/sequence.md',
-                            supports: ['sequence'],
+                            supports: ['sequence'],  // 该页面支持 sequence，可用的有[sequence, chart, flow, math, tree]
                         },
                         {
                             name: 'chart',
@@ -266,13 +227,53 @@ const config = {
                 },
             ],
         },
+        { // 第二个 menu
+            name: '文件夹展示1', // 如果没有该值，则为文件夹的名称 (expand 为 true 时无效)
+            folder: 'dir/menu', // 指定的路径
+            static: false, // 是否为static里面的文件
+            expand: false, // 如果为true,则该文件夹中的子文件夹在顶部展示为各个 menuItem，此时 name 无效，否则，以 name 为 menuItem，子文件夹为 group
+            // origin: 'https://raw.githubusercontent.com/blogoo/photo/master/', // 原始文件的链接
+        },
         {
+            name: '文件夹展示2',
+            groups: [
+                {
+                    name: '文件夹展示实例1', // 如果没有该值，则为文件夹的名称 (expand 为 true 时无效)
+                    folder: 'dir/group', // 指定的路径
+                    static: false, // 是否为static里面的文件
+                    expand: false, // 如果为true,则该文件夹中的子文件夹在顶部展示为各个 group，此时 name 无效，否则，以 name 为 group，子文件夹为 group 的内容
+                    // origin: 'https://raw.githubusercontent.com/blogoo/photo/master/', // 原始文件的链接
+                },
+                {
+                    name: '文件夹展示实例2',
+                    pages: [
+                        {
+                            name: 'fragment',
+                            path: 'md/fragment.md',
+                        },
+                        {
+                            name: '文件夹展示实例3', // 如果没有该值，则为文件夹的名称 (expand 为 true 时无效)
+                            folder: 'dir/page', // 指定的路径
+                            static: false, // 是否为static里面的文件
+                            expand: true, // 如果为true,则该文件夹中的子文件夹在顶部展示为各个 page，此时 name 无效，否则，以 name 为 page，子文件夹为 page 的内容
+                            // origin: 'https://raw.githubusercontent.com/blogoo/photo/master/', // 原始文件的链接
+                        },
+                        {
+                            name: 'fragment',
+                            path: 'md/fragment.md',
+                        },
+                    ]
+                },
+            ]
+        },
+        {
+            // 这个 menu item 没有 mainPage 和 groups， 但是拥有 pages，会以`下拉菜单`的方式，如果需要有树形目录的页面建议使用这种方式
             name: 'API',
             pages: [
                 {
                     name: 'pdshop',
                     path: 'api/pdshop.md',
-                    supports: ['tree'],
+                    supports: ['tree'], //是否支持树形结构
                 },
                 {
                     name: 'pdclient',
