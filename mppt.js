259773e83ae5abb25573b06e2888cc6b3ca00a8c
fov42550564 <42550564@qq.com>
2019-12-18 11:50:56 +0800

    修正错误

--- mppt.js
+++ mppt.js
@@ -1,51 +1,31 @@
-const markdownPlugins = require('./lib/markdownPlugins');
-
-const config = {
+module.exports = {
     projectName: 'mdoc-ppt',
-    title: '四面通物流大超市',
+    title: '酷炫H5',
     favicon: 'img/favicon.ico',
     logo: 'img/logo.png',
-    colors: {
-        primaryColor: 'rgb(34, 34, 34)',
-        secondaryColor: '#FF8C00',
-        activeColor: '#FF4040',
-        tintColor: '#005068',
-    },
-    highlight: {
-        theme: 'solarized-dark',
-    },
-    markdownPlugins,
-    documentPath: 'mppt', //默认为doc
-    styles: [],
-    scripts: [],
-    transition: 'slide', // none fade slide convex concave
+    documentPath: 'mppt',
+    transition: 'slide',
     backgroundImage: 'https://cdnoss.zhizuoh5.com/mkfiles/pic/5d9d42c572dec.png',
     arrowColor: '#FFF',
-    dist: '1.html', // 编译的目标文件
+    dist: '1.html',
     pages: [
         {
             name: '第一页',
             path: '1.md',
-        },
-        {
+        }, {
             name: '第二页',
             path: '2.md',
             transition: 'zoom',
             arrowColor: 'blue',
-        },
-        {
+        }, {
             name: '第三页',
             path: '3.md',
-        },
-        {
+        }, {
             name: '第四页',
             path: '4.md',
-        },
-        {
+        }, {
             name: '第五页',
             path: '5.md',
         },
     ],
 };
-
-module.exports = config;
