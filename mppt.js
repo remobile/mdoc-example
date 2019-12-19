46dc33326e145b6eb3eda8e2f9817c05458c48f5
fov42550564 <42550564@qq.com>
2019-12-19 14:31:52 +0800

    添加backgroundMusic

--- mppt.js
+++ mppt.js
@@ -5,28 +5,28 @@ module.exports = {
     logo: 'img/logo.png',
     documentPath: 'mppt',
     imagePath: 'mppt',
+    audioPath: 'audio',
     transition: 'slide',
     backgroundImage: 'mppt/5d9d42c572dec.png',
+    backgroundMusic: 'audio/mppt.mp3',
     arrowColor: '#FFF',
     dist: '1.html',
-    pages: [
-        {
-            name: '第1页',
-            path: '1.md',
-        }, {
-            name: '第2页',
-            path: '2.md',
-            transition: 'zoom',
-            arrowColor: 'blue',
-        }, {
-            name: '第3页',
-            path: '3.md',
-        }, {
-            name: '第4页',
-            path: '4.md',
-        }, {
-            name: '第5页',
-            path: '5.md',
-        },
-    ],
+    pages: [ {
+        name: '第1页',
+        path: '1.md',
+    }, {
+        name: '第2页',
+        path: '2.md',
+        transition: 'zoom',
+        arrowColor: 'blue',
+    }, {
+        name: '第3页',
+        path: '3.md',
+    }, {
+        name: '第4页',
+        path: '4.md',
+    }, {
+        name: '第5页',
+        path: '5.md',
+    } ],
 };
