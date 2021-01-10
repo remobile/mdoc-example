b849b014c46408a328cd874214605213fdd55637
fov42550564 <42550564@qq.com>
2021-01-10 19:38:24 +0800

    修正test

--- test.js
+++ test.js
@@ -1,13 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
+const {
+    Document,
+    HeadingLevel,
+    Packer,
+    Paragraph,
+    TextRun,
+}  = require( "./docx");
 
-function crateWordLayer(dir, level = -1) {
+function crateWordLayer(dir, children, level = -1) {
     fs.readdirSync(dir).forEach((file, index) => {
         const fullname = path.join(dir, file);
         const info = fs.statSync(fullname);
         if(info.isDirectory()) {
-            crateWordLayer(fullname, level + 1);
+            crateWordLayer(fullname, children, level + 1);
         } else if (/.*\.md/.test(file)) {
             const text = fs.readFileSync(fullname, 'utf8');
             const list = text.split(/\n/);
@@ -15,12 +22,39 @@ function crateWordLayer(dir, level = -1) {
                 if (/^#+\s+/.test(line)) {
                     const li = line.replace(/(^#+)[^#]*/, '$1');
                     const no = li.length + Math.max(level, 0);
-                    console.log(`${no}:${_.repeat('-', no*3)}${line.replace(/^#+\s+/, '')}`);
+                    const title = line.replace(/^#+\s+/, '');
+                    console.log(`HEADING_${no}:${_.repeat('-', no*3)}${title}`);
+                    children.push(new Paragraph({
+                        text: title,
+                        heading: HeadingLevel[`HEADING_${no}`],
+                    }));
                 } else {
-                    console.log(line);
+                    children.push(new Paragraph({ children: [new TextRun({
+                        text: line,
+                        color: 'red',
+                        size: 28,
+                        font: { name : 'Songti SC Regular' },
+                    })], indent: { left: 900, hanging: 360 } }));
                 }
             }
         }
     });
 }
-crateWordLayer('word');
+function main() {
+    const styles = fs.readFileSync("./res/styles.xml", "utf-8");
+    const numbering = fs.readFileSync("./res/numbering.xml", "utf-8");
+    const doc = new Document({
+        title: "计划书",
+        background: { color: "FFFFFF" },
+        externalStyles: styles,
+        externalNumbering: numbering,
+    });
+    const children = [];
+    crateWordLayer('word', children);
+    doc.addSection({ children });
+    Packer.toBuffer(doc).then((buffer) => {
+        fs.writeFileSync("test.docx", buffer);
+    });
+}
+
+main();
