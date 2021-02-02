3bf10a77fbecee31ca905a99f61db3be8e0ec185
fov42550564 <42550564@qq.com>
2021-02-02 23:31:26 +0800

    添加wordConfig

---
+++ wordConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: "计划书",
+    srcPath: 'word',
+    distPath: "test.docx",
+    backgroundCcolor: 'FFFFFF',
+    stylesPath: "./word/res/styles.xml",
+    numberingPath: "./word/res/numbering.xml",
+};
