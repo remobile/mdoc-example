98356b9a2b2544b259a4c6f69163096f41066327
fov42550564 <42550564@qq.com>
2021-02-09 22:20:01 +0800

    增加字体配置

--- wordConfig.js
+++ wordConfig.js
@@ -2,7 +2,11 @@ module.exports = {
     title: "计划书",
     srcPath: 'word',
     distPath: "test.docx",
-    backgroundCcolor: 'FFFFFF',
+    backgroundColor: 'FFFFFF', // 背景色
     stylesPath: "./word/res/styles.xml",
     numberingPath: "./word/res/numbering.xml",
+    fontName: 'Songti SC Regular', // 字体的名称
+    tableFontSize: 20, // 表格的文字的字体大小
+    imageTextFontSize: 20, // 图片下面的文字的字体大小
+    tableFontSize: 20, // 表格的文字的字体大小
 };
