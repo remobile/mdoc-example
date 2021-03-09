92fb1ca715650e29f793e606b521335d6312ad7f
fov42550564 <42550564@qq.com>
2021-03-09 20:09:58 +0800

    修正样式

--- wordConfig.js
+++ wordConfig.js
@@ -1,12 +1,11 @@
 module.exports = {
-    title: "计划书",
+    title: '计划书',
     srcPath: 'word',
-    distPath: "test.docx",
+    distPath: 'test.docx',
     backgroundColor: 'FFFFFF', // 背景色
-    stylesPath: "./word/res/styles.xml",
-    numberingPath: "./word/res/numbering.xml",
+    stylesPath: './word/res/styles.xml',
+    numberingPath: './word/res/numbering.xml',
     fontName: 'Songti SC Regular', // 字体的名称
-    tableFontSize: 20, // 表格的文字的字体大小
-    imageTextFontSize: 20, // 图片下面的文字的字体大小
-    tableFontSize: 20, // 表格的文字的字体大小
+    fontSize: 28, // 字体大小
+    tableFontSize: 28, // 表格的文字的字体大小
 };
