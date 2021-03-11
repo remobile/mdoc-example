ce7f1970ed4c2579a918a6eb2a4ae34d46e1b65c
fov42550564 <42550564@qq.com>
2021-03-11 22:51:35 +0800

    支持wps

--- wordConfig.js
+++ wordConfig.js
@@ -5,7 +5,8 @@ module.exports = {
     backgroundColor: 'FFFFFF', // 背景色
     stylesPath: './word/res/styles.xml',
     numberingPath: './word/res/numbering.xml',
-    fontName: 'Songti SC Regular', // 字体的名称
+    fontName: '宋体', // 字体的名称
     fontSize: 28, // 字体大小
     tableFontSize: 28, // 表格的文字的字体大小
+    wps: true, // 是否是wps
 };
